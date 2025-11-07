111e5c16ac1fddb7fa2176dc1eb664b70a341386
a.espinel <a.espinel@opendeusto.es>
2025-11-07 11:21:03 +0100

    Creación de clases Ruta, Camion, Tablet

---
+++ src/main/java/es/deusto/sd/ecoembes/entity/Camion.java
@@ -0,0 +1,36 @@
+package es.deusto.sd.ecoembes.entity;
+
+import es.deusto.sd.ecoembes.entity.Estado;
+
+import java.util.Objects;
+
+public class Camion {
+	
+	private String matricula;
+	private Double capacidadCarga;
+
+    public Camion(String matricula, Double capacidadCarga) {
+        this.matricula = matricula;
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public Camion() {
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Double getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(Double capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+
+}
